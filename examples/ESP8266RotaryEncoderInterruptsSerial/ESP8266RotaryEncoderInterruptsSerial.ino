44bdc5dfc3ed1a62b4194d1a70ff5df8f66eab2b
enjoyneering <enjoyneering79@gmail.com>
2017-04-25 20:29:47 -0400

    Update ESP8266RotaryEncoderInterruptsSerial.ino

--- examples/ESP8266RotaryEncoderInterruptsSerial/ESP8266RotaryEncoderInterruptsSerial.ino
+++ examples/ESP8266RotaryEncoderInterruptsSerial/ESP8266RotaryEncoderInterruptsSerial.ino
@@ -35,28 +35,22 @@ int16_t position = 0;
 RotaryEncoder encoder(PIN_A, PIN_B, BUTTON);
 
 
-void encoderISRa()
+void encoderISR()
 {
-  encoder.updateA();
+  encoder.readAB();
 }
 
-void encoderISRb()
+void pushButtonISR()
 {
-  encoder.updateB();
-}
-
-void encoderISRpb()
-{
-  encoder.updatePB();
+  encoder.readPushButton();
 }
 
 void setup()
 {
   encoder.begin();
 
-  attachInterrupt(digitalPinToInterrupt(PIN_A),  encoderISRa,  CHANGE); //call encoderISRa()  when any high/low changes happen on the pin
-  attachInterrupt(digitalPinToInterrupt(PIN_B),  encoderISRb,  CHANGE); //call encoderISRb()  when any high/low changes happen on the pin
-  attachInterrupt(digitalPinToInterrupt(BUTTON), encoderISRpb, LOW);    //call encoderISRpb() when low changes happen on the pin
+  attachInterrupt(digitalPinToInterrupt(PIN_A),  encoderISR,    FALLING); //call encoderISR()    when high->low changes happened
+  attachInterrupt(digitalPinToInterrupt(BUTTON), pushButtonISR, FALLING); //call pushButtonISR() when high->low changes happened
 
   Serial.begin(115200);
 }
@@ -64,13 +58,13 @@ void setup()
 
 void loop()
 {
-  if (position != encoder.readPosition())
+  if (position != encoder.getPosition())
   {
-    position = encoder.readPosition();
+    position = encoder.getPosition();
     Serial.println(position);
   }
   
-  if (encoder.readPushButton() == LOW)
+  if (encoder.getPushButton() == LOW)
   {
     Serial.println("PRESSED");
   }
