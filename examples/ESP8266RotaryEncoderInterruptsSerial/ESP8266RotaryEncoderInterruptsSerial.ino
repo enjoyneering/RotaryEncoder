9254ee90137103a98a97453192f746a4ff85cc5b
enjoyneering <enjoyneering79@gmail.com>
2018-06-17 13:32:09 -0400

    Update ESP8266RotaryEncoderInterruptsSerial.ino

--- examples/ESP8266RotaryEncoderInterruptsSerial/ESP8266RotaryEncoderInterruptsSerial.ino
+++ examples/ESP8266RotaryEncoderInterruptsSerial/ESP8266RotaryEncoderInterruptsSerial.ino
@@ -5,21 +5,22 @@
   written by : enjoyneering79
   sourse code: https://github.com/enjoyneering/
 
-  This sketch uses interrupts, specials pins are required to interface
+  This library uses interrupts, specials pins are required to interface
   Board:                                    int.0  int.1  int.2  int.3  int.4  int.5
-  Uno, Mini, Pro (ATmega168 & ATmega328)... 2      3      x       x      x     x
+  Uno, Mini, Pro, ATmega168, ATmega328..... 2      3      x       x      x     x
   Mega2560................................. 2      3      21      20     19    18
-  Leonardo, Micro (ATmega32U4)............. 3      2      0       1      7     x
+  Leonardo, Micro, ATmega32U4.............. 3      2      0       1      7     x
+  Digistump, Trinket, ATtiny85............. 2/physical pin no.7
   Due...................................... all digital pins
   Zero..................................... all digital pins, except pin 4
-  Arduino STM32/STM32F103xxxx boards....... all digital pins, maximun 16 pins at the same time
+  Blue Pill, STM32F103xxxx boards.......... all digital pins, maximun 16 pins at the same time
   ESP8266.................................. all digital pins, except gpio6 - gpio11 & gpio16
 
   Frameworks & libraries:
-  ATtiny Core           - https://github.com/SpenceKonde/ATTinyCore 
-  ATtiny I2C Master lib - https://github.com/SpenceKonde/TinyWireM
-  ESP8266 Core          - https://github.com/esp8266/Arduino
-  Arduino STM32 Core    - https://github.com/rogerclarkmelbourne/Arduino_STM32
+  TimerOne AVR - https://github.com/PaulStoffregen/TimerOne
+  ATtiny  Core - https://github.com/SpenceKonde/ATTinyCore 
+  ESP8266 Core - https://github.com/esp8266/Arduino
+  STM32   Core - https://github.com/rogerclarkmelbourne/Arduino_STM32
 
   NOTE:
   - LOW     interrupt trigges whenever the pin is low
@@ -35,9 +36,9 @@
 #include <RotaryEncoder.h>
 #include <ESP8266WiFi.h>
 
-#define PIN_A      D5 //ky-040 clk pin, interrupt, add 100nF/0.1uF capacitors between pin & ground
-#define PIN_B      D6 //ky-040 dt  pin,            add 100nF/0.1uF capacitors between pin & ground
-#define BUTTON     D7 //ky-040 sw  pin, interrupt
+#define PIN_A   D5 //ky-040 clk pin, interrupt, add 100nF/0.1uF capacitors between pin & ground
+#define PIN_B   D6 //ky-040 dt  pin,            add 100nF/0.1uF capacitors between pin & ground
+#define BUTTON  D7 //ky-040 sw  pin, interrupt
 
 int16_t position = 0;
 
@@ -75,5 +76,5 @@ void loop()
     Serial.println(position);
   }
   
-  if (encoder.getPushButton() == true) Serial.println("PRESSED");
+  if (encoder.getPushButton() == true) Serial.println(F("PRESSED"));         //(F()) saves string to flash & keeps dynamic memory free
 }
