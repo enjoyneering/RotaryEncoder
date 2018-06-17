5fb9e2dc1df0cd66fad3bc677298ac2f77bebbb5
enjoyneering <enjoyneering79@gmail.com>
2018-06-17 13:34:58 -0400

    Create STM32RotaryEncoderInterruptsSerial.ino

---
+++ examples/STM32RotaryEncoderInterruptsSerial/STM32RotaryEncoderInterruptsSerial.ino
@@ -0,0 +1,76 @@
+/***************************************************************************************************/
+/*
+  This is an Arduino sketch for RotaryEncoder library using interrupts
+
+  written by : enjoyneering79
+  sourse code: https://github.com/enjoyneering/
+
+  This library uses interrupts, specials pins are required to interface
+  Board:                                    int.0  int.1  int.2  int.3  int.4  int.5
+  Uno, Mini, Pro, ATmega168, ATmega328..... 2      3      x       x      x     x
+  Mega2560................................. 2      3      21      20     19    18
+  Leonardo, Micro, ATmega32U4.............. 3      2      0       1      7     x
+  Digistump, Trinket, ATtiny85............. 2/physical pin no.7
+  Due...................................... all digital pins
+  Zero..................................... all digital pins, except pin 4
+  Blue Pill, STM32F103xxxx boards.......... all digital pins, maximun 16 pins at the same time
+  ESP8266.................................. all digital pins, except gpio6 - gpio11 & gpio16
+
+  Frameworks & libraries:
+  TimerOne AVR - https://github.com/PaulStoffregen/TimerOne
+  ATtiny  Core - https://github.com/SpenceKonde/ATTinyCore 
+  ESP8266 Core - https://github.com/esp8266/Arduino
+  STM32   Core - https://github.com/rogerclarkmelbourne/Arduino_STM32
+
+  NOTE:
+  - LOW     interrupt trigges whenever the pin is low
+  - HIGH    interrupt triggers whenever the pin is high (Arduino Due, Zero, MKR1000 only)
+  - CHANGE  interrupt triggers whenever the pin changes value
+  - RISING  interrupt triggers when the pin goes from low to high
+  - FALLING interrupt triggers when the pin goes from high to low
+
+  GNU GPL license, all text above must be included in any redistribution, see link below for details:
+  - https://www.gnu.org/licenses/licenses.html
+*/
+/***************************************************************************************************/
+#include <RotaryEncoder.h>
+
+#define PIN_A   B13 //ky-040 clk pin, add 100nF/0.1uF capacitors between pin & ground
+#define PIN_B   B14 //ky-040 dt  pin, add 100nF/0.1uF capacitors between pin & ground
+#define BUTTON  B15 //ky-040 sw  pin
+
+int16_t position = 0;
+
+RotaryEncoder encoder(PIN_A, PIN_B, BUTTON);
+
+
+void encoderISR()
+{
+  encoder.readAB();
+}
+
+void encoderButtonISR()
+{
+  encoder.readPushButton();
+}
+
+void setup()
+{
+  encoder.begin();                                                           //set encoders pins as input & enable built-in pullup resistors
+
+  attachInterrupt(digitalPinToInterrupt(PIN_A),  encoderISR,       CHANGE);  //call encoderISR()       when high->low or high->low changes happened
+  attachInterrupt(digitalPinToInterrupt(BUTTON), encoderButtonISR, FALLING); //call encoderButtonISR() when high->low changes happened
+
+  Serial.begin(115200);
+}
+
+void loop()
+{
+  if (position != encoder.getPosition())
+  {
+    position = encoder.getPosition();
+    Serial.println(position);
+  }
+  
+  if (encoder.getPushButton() == true) Serial.println(F("PRESSED"));         //(F()) saves string to flash & keeps dynamic memory free
+}
