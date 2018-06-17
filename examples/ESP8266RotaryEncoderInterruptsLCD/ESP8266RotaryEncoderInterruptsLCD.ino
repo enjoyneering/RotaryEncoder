d5b7c42d755a8badcf424abb5f881cf44dfd8b21
enjoyneering <enjoyneering79@gmail.com>
2018-06-17 13:25:45 -0400

    Update ESP8266RotaryEncoderInterruptsLCD.ino

--- examples/ESP8266RotaryEncoderInterruptsLCD/ESP8266RotaryEncoderInterruptsLCD.ino
+++ examples/ESP8266RotaryEncoderInterruptsLCD/ESP8266RotaryEncoderInterruptsLCD.ino
@@ -5,31 +5,35 @@
   written by : enjoyneering79
   sourse code: https://github.com/enjoyneering/
 
-  This sketch uses interrupts, specials pins are required to interface
+  This library uses interrupts, specials pins are required to interface
   Board:                                    int.0  int.1  int.2  int.3  int.4  int.5
-  Uno, Mini, Pro (ATmega168 & ATmega328)... 2      3      x       x      x     x
+  Uno, Mini, Pro, ATmega168, ATmega328..... 2      3      x       x      x     x
   Mega2560................................. 2      3      21      20     19    18
-  Leonardo, Micro (ATmega32U4)............. 3      2      0       1      7     x
+  Leonardo, Micro, ATmega32U4.............. 3      2      0       1      7     x
+  Digistump, Trinket, ATtiny85............. 2/physical pin no.7
   Due...................................... all digital pins
   Zero..................................... all digital pins, except pin 4
-  Arduino STM32/STM32F103xxxx boards....... all digital pins, maximun 16 pins at the same time
+  Blue Pill, STM32F103xxxx boards.......... all digital pins, maximun 16 pins at the same time
   ESP8266.................................. all digital pins, except gpio6 - gpio11 & gpio16
 
   PCF8574 chip uses I2C bus to communicate, specials pins are required to interface
   Board:                                    SDA                    SCL
-  Uno, Mini, Pro........................... A4                     A5
+  Uno, Mini, Pro, ATmega168, ATmega328..... A4                     A5
   Mega2560, Due............................ 20                     21
   Leonardo, Micro, ATmega32U4.............. 2                      3
   Digistump, Trinket, ATtiny85............. 0/physical pin no.5    2/physical pin no.7
-  Blue Pill, STM32F103xxxx boards.......... B7                     B6 with 5v->3v logich converter
+  Blue Pill, STM32F103xxxx boards.......... B7*                    B6*
   ESP8266 ESP-01:.......................... GPIO0/D5               GPIO2/D3
   NodeMCU 1.0, WeMos D1 Mini............... GPIO4/D2               GPIO5/D1
 
+                                            *STM32F103xxxx pins B7/B7 are 5v tolerant, but bi-directional
+                                             logic level converter is recommended
+
   Frameworks & libraries:
-  ATtiny Core           - https://github.com/SpenceKonde/ATTinyCore 
-  ATtiny I2C Master lib - https://github.com/SpenceKonde/TinyWireM
-  ESP8266 Core          - https://github.com/esp8266/Arduino
-  Arduino STM32 Core    - https://github.com/rogerclarkmelbourne/Arduino_STM32
+  TimerOne AVR - https://github.com/PaulStoffregen/TimerOne
+  ATtiny  Core - https://github.com/SpenceKonde/ATTinyCore 
+  ESP8266 Core - https://github.com/esp8266/Arduino
+  STM32   Core - https://github.com/rogerclarkmelbourne/Arduino_STM32
 
   NOTE:
   - LOW     interrupt trigges whenever the pin is low
@@ -42,8 +46,8 @@
   - https://www.gnu.org/licenses/licenses.html
 */
 /***************************************************************************************************/
-#include <LiquidCrystal_I2C.h> //https://github.com/enjoyneering/LiquidCrystal_I2C
 #include <Wire.h>              //https://github.com/enjoyneering/ESP8266-I2C-Driver
+#include <LiquidCrystal_I2C.h> //https://github.com/enjoyneering/LiquidCrystal_I2C
 #include <ESP8266WiFi.h>
 #include <RotaryEncoder.h>
 
@@ -87,23 +91,23 @@ void setup()
   /* LCD connection check */ 
   while (lcd.begin(LCD_COLUMNS, LCD_ROWS, LCD_5x8DOTS, D2, D1) != true)      //colums - 20, rows - 4, pixels - LCD_5x8DOTS, SDA - D2, SCL - D1
   {
-    Serial.println("PCF8574 is not connected or lcd pins declaration is wrong. Only pins numbers: 4,5,6,16,11,12,13,14 are legal.");
+    Serial.println(F("PCF8574 is not connected or lcd pins declaration is wrong. Only pins numbers: 4,5,6,16,11,12,13,14 are legal.")); //(F()) saves string to flash & keeps dynamic memory free
     delay(5000);
   }
 
-  lcd.print("PCF8574 is OK");
+  lcd.print(F("PCF8574 is OK"));
   delay(1500);
 
   lcd.clear();
 
   /* prints static text */
-  lcd.print("POSITION:");
+  lcd.print(F("POSITION:"));
   lcd.setCursor(0, 1);
-  lcd.print("BUTTON  :");
+  lcd.print(F("BUTTON  :"));
   lcd.setCursor(10, 1);
   lcd.print(buttonCounter);
   lcd.setCursor(0, 2);
-  lcd.print("UPTIME  :");
+  lcd.print(F("UPTIME  :"));
 }
 
 void loop()
@@ -117,7 +121,7 @@ void loop()
     lcd.setCursor(10, 1);
     lcd.print(buttonCounter++);
 
-    if   (buttonCounter % 4 == 0) lcd.noBacklight();
+    if   (buttonCounter % 4 == 0) lcd.noBacklight();                         //every 4-th button click the backlight is off
     else                          lcd.backlight();
   }
 
