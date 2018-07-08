013bfe1a7ef5a5dae24b9688b38749ac7e87e384
enjoyneering <enjoyneering79@gmail.com>
2018-07-08 15:41:06 -0400

    Create ESP8266RotaryEncoderAdvancedInterruptsLCD.ino

---
+++ examples/ESP8266RotaryEncoderAdvancedInterruptsLCD/ESP8266RotaryEncoderAdvancedInterruptsLCD.ino
@@ -0,0 +1,130 @@
+/***************************************************************************************************/
+/*
+  This is an Arduino sketch for RotaryEncoder library using interrupts
+
+  written by : enjoyneering79
+  sourse code: https://github.com/enjoyneering/
+
+  This library uses interrupts, specials pins are required to interface
+  Board:                                    int.0  int.1  int.2  int.3  int.4  int.5
+  Uno, Mini, Pro, ATmega168, ATmega328..... 2      3      x       x      x     x
+  Mega2560................................. 2      3      21      20     19    18
+  Leonardo, Micro, ATmega32U4.............. 3      2      0       1      7     x
+  Digistump, Trinket, ATtiny85............. 2/physical pin no.7
+  Due...................................... all digital pins
+  Zero..................................... all digital pins, except pin 4
+  Blue Pill, STM32F103xxxx boards.......... all digital pins, maximun 16 pins at the same time
+  ESP8266.................................. all digital pins, except gpio6 - gpio11 & gpio16
+
+  PCF8574 chip uses I2C bus to communicate, specials pins are required to interface
+  Board:                                    SDA                    SCL
+  Uno, Mini, Pro, ATmega168, ATmega328..... A4                     A5
+  Mega2560, Due............................ 20                     21
+  Leonardo, Micro, ATmega32U4.............. 2                      3
+  Digistump, Trinket, ATtiny85............. 0/physical pin no.5    2/physical pin no.7
+  Blue Pill, STM32F103xxxx boards.......... PB7*                   PB6*
+  ESP8266 ESP-01:.......................... GPIO0/D5               GPIO2/D3
+  NodeMCU 1.0, WeMos D1 Mini............... GPIO4/D2               GPIO5/D1
+
+                                           *STM32F103xxxx pins PB6/PB7 are 5v tolerant, but
+                                            bi-directional logic level converter is recommended
+
+  Frameworks & libraries:
+  TimerOne AVR - https://github.com/PaulStoffregen/TimerOne
+  ATtiny  Core - https://github.com/SpenceKonde/ATTinyCore 
+  ESP8266 Core - https://github.com/esp8266/Arduino
+  STM32   Core - https://github.com/rogerclarkmelbourne/Arduino_STM32
+
+  NOTE:
+  - LOW     interrupt trigges whenever the pin is low
+  - HIGH    interrupt triggers whenever the pin is high (Arduino Due, Zero, MKR1000 only)
+  - CHANGE  interrupt triggers whenever the pin changes value
+  - RISING  interrupt triggers when the pin goes from low to high
+  - FALLING interrupt triggers when the pin goes from high to low
+
+  GNU GPL license, all text above must be included in any redistribution, see link below for details:
+  - https://www.gnu.org/licenses/licenses.html
+*/
+/***************************************************************************************************/
+#include <Wire.h>                  //use bug free i2c driver https://github.com/enjoyneering/ESP8266-I2C-Driver
+#include <ESP8266WiFi.h>
+#include <LiquidCrystal_I2C.h>     //https://github.com/enjoyneering/LiquidCrystal_I2C
+#include <RotaryEncoderAdvanced.h>
+
+#define LCD_ROWS         4     //qnt. of lcd rows
+#define LCD_COLUMNS      20    //qnt. of lcd columns
+
+#define LCD_SPACE_SYMBOL 0x20  //space symbol from the LCD ROM, see p.9 of GDM2004D datasheet
+
+#define PIN_A            D5    //ky-040 clk pin, interrupt, add 100nF/0.1uF capacitors between pin & ground
+#define PIN_B            D6    //ky-040 dt  pin,            add 100nF/0.1uF capacitors between pin & ground
+#define BUTTON           D7    //ky-040 sw  pin, interrupt
+
+uint16_t buttonCounter = 0;
+
+RotaryEncoderAdvanced encoder(PIN_A, PIN_B, BUTTON, 0.1, 0.0, 3.3); //0.1 step per click, minimum value 0, maximum value 3.3
+LiquidCrystal_I2C lcd(PCF8574_ADDR_A21_A11_A01, 4, 5, 6, 16, 11, 12, 13, 14, POSITIVE);
+
+
+void encoderISR()
+{
+  encoder.readAB();
+}
+
+void encoderButtonISR()
+{
+  encoder.readPushButton();
+}
+
+void setup()
+{
+  WiFi.persistent(false);                                                    //disable saving wifi config into SDK flash area
+  WiFi.forceSleepBegin();                                                    //disable AP & station by calling "WiFi.mode(WIFI_OFF)" & put modem to sleep
+  
+  encoder.begin();                                                           //set encoders pins as input & enable built-in pullup resistors
+
+  attachInterrupt(digitalPinToInterrupt(PIN_A),  encoderISR,       CHANGE);  //call encoderISR()    when high->low or high->low changes happened
+  attachInterrupt(digitalPinToInterrupt(BUTTON), encoderButtonISR, FALLING); //call pushButtonISR() when high->low changes happened
+
+  Serial.begin(115200);
+
+  /* LCD connection check */ 
+  while (lcd.begin(LCD_COLUMNS, LCD_ROWS, LCD_5x8DOTS, D2, D1) != true)      //colums - 20, rows - 4, pixels - LCD_5x8DOTS, SDA - D2, SCL - D1
+  {
+    Serial.println(F("PCF8574 is not connected or lcd pins declaration is wrong. Only pins numbers: 4,5,6,16,11,12,13,14 are legal.")); //(F()) saves string to flash & keeps dynamic memory free
+    delay(5000);
+  }
+
+  lcd.print(F("PCF8574 is OK"));
+  delay(1500);
+
+  lcd.clear();
+
+  /* prints static text */
+  lcd.print(F("VALUE :"));
+  lcd.setCursor(0, 1);
+  lcd.print(F("BUTTON:"));
+  lcd.setCursor(8, 1);
+  lcd.print(buttonCounter);
+  lcd.setCursor(0, 2);
+  lcd.print(F("UPTIME:"));
+}
+
+void loop()
+{
+  lcd.setCursor(8, 0);
+  lcd.print(encoder.getValue(), 1);
+  lcd.write(LCD_SPACE_SYMBOL);
+
+  if (encoder.getPushButton() == true)
+  {
+    lcd.setCursor(8, 1);
+    lcd.print(buttonCounter++);
+
+    if   (buttonCounter % 4 == 0) lcd.noBacklight();                         //every 4-th button click the backlight is off
+    else                          lcd.backlight();
+  }
+
+  lcd.setCursor(8, 2);
+  lcd.print((millis() / 1000));
+}
