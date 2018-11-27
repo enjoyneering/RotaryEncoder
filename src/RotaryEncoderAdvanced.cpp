34e984cc5ea7a8284e5f1b6daabeba69de794217
enjoyneering <enjoyneering79@gmail.com>
2018-11-26 19:41:34 -0500

    Update RotaryEncoderAdvanced.cpp

--- src/RotaryEncoderAdvanced.cpp
+++ src/RotaryEncoderAdvanced.cpp
@@ -1,29 +1,24 @@
 /***************************************************************************************************/
 /*
-  This is an Arduino library for Quadrature Rotary Encoder 
+   This is an Arduino library for Quadrature Rotary Encoder 
 
-  written by : enjoyneering79
-  sourse code: https://github.com/enjoyneering/
+   written by : enjoyneering79
+   sourse code: https://github.com/enjoyneering/
 
-  This library uses interrupts, specials pins are required to interface
-  Board:                                    int.0  int.1  int.2  int.3  int.4  int.5
-  Uno, Mini, Pro, ATmega168, ATmega328..... 2      3      x       x      x     x
-  Mega2560................................. 2      3      21      20     19    18
-  Leonardo, Micro, ATmega32U4.............. 3      2      0       1      7     x
-  Digistump, Trinket, ATtiny85............. 2/physical pin no.7
-  Due...................................... all digital pins
-  Zero..................................... all digital pins, except pin 4
-  Blue Pill, STM32F103xxxx boards.......... all digital pins, maximun 16 pins at the same time
-  ESP8266.................................. all digital pins, except gpio6 - gpio11 & gpio16
+   This library uses interrupts, specials pins are required to interface
+   Board:                                    int.0  int.1  int.2  int.3  int.4  int.5            Level
+   Uno, Mini, Pro, ATmega168, ATmega328..... 2      3      x       x      x     x                5v
+   Mega2560................................. 2      3      21      20     19    18               5v
+   Leonardo, Micro, ATmega32U4.............. 3      2      0       1      7     x                5v
+   Digistump, Trinket, ATtiny85............. 2/physical pin 7                                    5v
+   Due, SAM3X8E............................. all digital pins                                    3v
+   Zero, ATSAMD21G18........................ all digital pins, except pin 4                      3v
+   Blue Pill, STM32F103xxxx boards.......... all digital pins, maximum 16 pins at the same time  3v
+   ESP8266.................................. all digital pins, except gpio6 - gpio11 & gpio16    3v/5v
+   ESP32.................................... all digital pins                                    3v
 
-  Frameworks & libraries:
-  TimerOne AVR - https://github.com/PaulStoffregen/TimerOne
-  ATtiny  Core - https://github.com/SpenceKonde/ATTinyCore 
-  ESP8266 Core - https://github.com/esp8266/Arduino
-  STM32   Core - https://github.com/rogerclarkmelbourne/Arduino_STM32
-
-  NOTE:
-  - Quadrature encoder makes two waveforms that are 90 deg. out of phase:
+   NOTE:
+   - Quadrature encoder makes two waveforms that are 90° out of phase:
                            _______         _______         __
                   PinA ___|       |_______|       |_______|   PinA
           CCW <--              _______         _______
@@ -56,31 +51,35 @@
             11         10            1110          1     CW,  0x0E
             11         11            1111          0     stop/idle
 
-   - CW  states 0b0001, 0b0111, 0b1000, 0b1110
-   - CСW states 0b0010, 0b0100, 0b1011, 0b1101
-   - for best result add 100nF/0.1uF capacitors between A & B channel pin & ground
+          - CW  states 0b0001, 0b0111, 0b1000, 0b1110
+          - CCW states 0b0010, 0b0100, 0b1011, 0b1101
 
-  GNU GPL license, all text above must be included in any redistribution, see link below for details:
-  - https://www.gnu.org/licenses/licenses.html
+   Frameworks & Libraries:
+   TimerOne AVR          - https://github.com/PaulStoffregen/TimerOne
+   ATtiny  Core          - https://github.com/SpenceKonde/ATTinyCore
+   ESP32   Core          - https://github.com/espressif/arduino-esp32
+   ESP8266 Core          - https://github.com/esp8266/Arduino
+   STM32   Core          - https://github.com/rogerclarkmelbourne/Arduino_STM32
+
+   GNU GPL license, all text above must be included in any redistribution,
+   see link for details  - https://www.gnu.org/licenses/licenses.html
 */
 /***************************************************************************************************/
 
 #include "RotaryEncoderAdvanced.h"
 
 
+
 /**************************************************************************/
 /*
     Constructor
-
-    NOTE:
-    - to do template <typename ANY_TYPE> RotaryEncoderAdvanced(ANY_TYPE varieble) 
 */
 /**************************************************************************/
-RotaryEncoderAdvanced::RotaryEncoderAdvanced(uint8_t encoderA, uint8_t encoderB, uint8_t encoderButton, float stepsPerClick, float minValue, float maxValue) : RotaryEncoder(encoderA, encoderB, encoderButton)
+template <typename T> RotaryEncoderAdvanced<T>::RotaryEncoderAdvanced(uint8_t encoderA, uint8_t encoderB, uint8_t encoderButton, T stepsPerClick, T minValue, T maxValue) : RotaryEncoder(encoderA, encoderB, encoderButton)
 {
-  _stepsPerClick = stepsPerClick;
-  _minValue      = minValue;
-  _maxValue      = maxValue;
+  setStepsPerClick(stepsPerClick);
+  setMinValue(minValue);
+  setMaxValue(maxValue);
 }
 
 /**************************************************************************/
@@ -90,16 +89,17 @@ RotaryEncoderAdvanced::RotaryEncoderAdvanced(uint8_t encoderA, uint8_t encoderB,
     Return encoder value
 */
 /**************************************************************************/
-float RotaryEncoderAdvanced::getValue()
+template <typename T> T RotaryEncoderAdvanced<T>::getValue()
 {
-  float value = (float)_counter * _stepsPerClick;
+  T value = (T)_counter * _stepsPerClick;
 
-  if (value > _maxValue)      //make shure that "value" never gets higher "maxValue"
+  /* make shure value never gets higher or lower thresholds */
+  if (value > _maxValue)
   {
     value = _maxValue;
     setValue(value);
   }
-  else if (value < _minValue) //make shure that "value" never gets lower  "minValue"
+  else if (value < _minValue)
   {
     value = _minValue;
     setValue(value);
@@ -115,7 +115,103 @@ float RotaryEncoderAdvanced::getValue()
     Manualy sets encoder value
 */
 /**************************************************************************/
-void RotaryEncoderAdvanced::setValue(float value)
+template <typename T> void RotaryEncoderAdvanced<T>::setValue(T value)
 {
-  _counter = (value / _stepsPerClick);
+  _counter = value / _stepsPerClick;
+}
+
+/**************************************************************************/
+/*
+    getStepsPerClick()
+
+    Return steps per click value
+*/
+/**************************************************************************/
+template <typename T> T RotaryEncoderAdvanced<T>::getStepsPerClick()
+{
+  return _stepsPerClick;
+}
+
+/**************************************************************************/
+/*
+    setStepsPerClick()
+
+    Set steps per click value
+
+    NOTE:
+    - TODO: #pragma gcc error ("division by zero is forbidden")
+*/
+/**************************************************************************/
+template <typename T> void RotaryEncoderAdvanced<T>::setStepsPerClick(T value)
+{
+  if (value == 0) return; //sanity check, division by zero is forbidden
+
+  _stepsPerClick = value;
+}
+
+/**************************************************************************/
+/*
+    getMinValue()
+
+    Return minimum value
+*/
+/**************************************************************************/
+template <typename T> T RotaryEncoderAdvanced<T>::getMinValue()
+{
+  return _minValue;
+}
+
+/**************************************************************************/
+/*
+    setMinValue()
+
+    Set minimum value
+*/
+/**************************************************************************/
+template <typename T> void RotaryEncoderAdvanced<T>::setMinValue(T value)
+{
+  _minValue = value;
+}
+
+/**************************************************************************/
+/*
+    getMaxValue()
+
+    Return maximun value
+*/
+/**************************************************************************/
+template <typename T> T RotaryEncoderAdvanced<T>::getMaxValue()
+{
+  return _maxValue;
+}
+
+/**************************************************************************/
+/*
+    setMaxValue()
+
+    Set maximun value
+*/
+/**************************************************************************/
+template <typename T> void RotaryEncoderAdvanced<T>::setMaxValue(T value)
+{
+  _maxValue = value;
+}
+
+/**************************************************************************/
+/*
+    setValues()
+
+    Set current value, steps per click, minimum & maximun values
+*/
+/**************************************************************************/
+template <typename T> void RotaryEncoderAdvanced<T>::setValues(T currValue, T stepsPerClick, T minValue, T maxValue)
+{
+  noInterrupts();                  //disable interrupts
+
+  setStepsPerClick(stepsPerClick);
+  setMinValue(minValue);
+  setMaxValue(maxValue);
+  setValue(currValue);
+
+  interrupts();                    //enable interrupts
 }
